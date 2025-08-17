0e4c3ce9f2b77d217c872027f8a05cda7665761c
ANKIT SINGH <148553061+Ankit-Singh1234@users.noreply.github.com>
2025-08-17 14:27:51 +0530

    Update longestCommonPrefix.cpp

--- String/longestCommonPrefix.cpp
+++ String/longestCommonPrefix.cpp
@@ -1,3 +1,51 @@
+///Method 1???
+class Solution {
+public:
+
+    string common(string s1, string s2){
+        string temp="";
+        int n=min(s1.length(), s2.length());
+        for(int i=0; i<n; i++){
+            if(s1[i]==s2[i]){
+                temp+=s1[i];
+            }
+            else{
+                break;
+            }
+        }
+
+        return temp;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        string res=strs[0];
+         /*
+    Initialize res with the first string: Instead of initializing res to an empty string, it  should start as the first string in the list (res = strs[0]). This ensures that the first string is the initial common prefix.
+    */
+        for(int i=0; i<n; i++){
+            res=common(res, strs[i]);
+        }
+
+        return res;
+    }
+
+   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Methoid 2?////
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& v) {
